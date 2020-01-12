2873f59641157b149c3b214f7416fafff19c6507
Marco Gerstmann <marco@devtypes.com>
2020-01-12 16:15:50 +0100

    [#170664209] Add modules for daily journal and habits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {DailyJournalComponent} from './daily-journal/daily-journal.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'daily-journal', pathMatch: 'full'},
+  {path: 'daily-journal', component: DailyJournalComponent},
+  {path: 'habits', loadChildren: () => import('./habit/habit.module').then(mod => mod.HabitModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/habit/habit-list/habit-list.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'dt-habit-list',
+  templateUrl: './habit-list.component.html',
+  styleUrls: ['./habit-list.component.scss']
+})
+export class HabitListComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
