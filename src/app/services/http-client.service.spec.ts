2e4988a62291971cdfb485696c5b27482d74c7b3
Marco Gerstmann <marco@devtypes.com>
2020-01-13 21:20:25 +0100

    Add more tests

--- src/app/services/http-client.service.spec.ts
+++ src/app/services/http-client.service.spec.ts
@@ -1,10 +1,10 @@
 import {TestBed} from '@angular/core/testing';
 import {HttpClient} from '@angular/common/http';
+import {noop, of, throwError} from 'rxjs';
 import {HttpClientService} from './http-client.service';
 import {Mock, MockingUtil} from '../testing/mocking.util';
 import {ConfigService} from './config.service';
 import {HttpExceptionHandlerService} from './http-exception-handler.service';
-import {of} from 'rxjs';
 
 describe('HttpClientService', () => {
   let service: HttpClientService;
@@ -31,9 +31,27 @@ describe('HttpClientService', () => {
     configMock.apiUrl = 'http://localhost:8080/api/';
   });
 
-  it('#get should do an http request', () => {
-    service.get('v1/test-endpoint');
-    expect(httpClientMock.request).toHaveBeenCalled();
+  describe('#get', () => {
+    it('should do an http request without http params', () => {
+      service.get('v1/test-endpoint');
+      expect(httpClientMock.request).toHaveBeenCalled();
+    });
+
+    it('should do an http request with http params', () => {
+      service.get('v1/test-endpoint', {});
+      expect(httpClientMock.request).toHaveBeenCalled();
+    });
+
+    it('should handle exceptions', (done) => {
+      const mockHandledError = of('A test error');
+      httpClientMock.request.and.returnValue(throwError('A test error'));
+      httpErrorServiceMock.handleError.and.returnValue(mockHandledError);
+      service.get('route').subscribe(noop, (error: any) => {
+        expect(httpErrorServiceMock.handleError).toHaveBeenCalledWith('A test error');
+        expect(error).toEqual(mockHandledError);
+        done();
+      });
+    });
   });
 
   it('#post should do an http request', () => {
--- src/app/services/logging.service.spec.ts
+++ src/app/services/logging.service.spec.ts
@@ -28,7 +28,6 @@ describe('LoggingService', () => {
   describe('while in a DEV environment', () => {
     beforeEach(() => {
       configServiceMock.env = 'DEV';
-      service = TestBed.get(LoggingService);
     });
 
     it('#debug should call console.debug', () => {
@@ -54,6 +53,37 @@ describe('LoggingService', () => {
     it('#fatal should call console.fatal', () => {
       service.fatal('testMessage');
       expect(console.error).toHaveBeenCalled();
+    });
+  });
+
+  describe('while in a non-DEV environment', () => {
+    beforeEach(() => {
+      configServiceMock.env = 'PROD';
+    });
+
+    it('#debug should call console.debug', () => {
+      service.debug('testMessage');
+      expect(console.debug).toHaveBeenCalled();
+    });
+
+    it('#info should NOT call console.info', () => {
+      service.info('testMessage');
+      expect(console.info).not.toHaveBeenCalled();
+    });
+
+    it('#warning should NOT call console.warn', () => {
+      service.warning('testMessage');
+      expect(console.warn).not.toHaveBeenCalled();
+    });
+
+    it('e#rror should NOT call console.error', () => {
+      service.error('testMessage');
+      expect(console.error).not.toHaveBeenCalled();
+    });
+
+    it('#fatal should NOT call console.fatal', () => {
+      service.fatal('testMessage');
+      expect(console.error).not.toHaveBeenCalled();
     });
   });
 });
